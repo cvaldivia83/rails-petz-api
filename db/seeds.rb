1ca760174fd900e5b2a5bc82e168ee4928b8a0ab
Carla Valdivia <valdivia.carla@gmail.com>
2024-10-01 13:26:15 -0300

    Basic seed for user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'open-uri'
+
+# user 
+user = User.new(
+  email: 'carla@lewagon.com',
+  password: '123456',
+  username: 'cvaldivia'
+)
+
+file = URI.parse("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg").open
+
+user.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+
+user.save!
