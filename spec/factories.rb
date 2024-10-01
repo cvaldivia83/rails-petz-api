06d7e22f4a61fa3efcda2c52d3afa9e2ad67e1ae
Carla Valdivia <valdivia.carla@gmail.com>
2024-10-01 18:09:40 -0300

    Add factory_bot + factories for users and posts

---
+++ spec/factories.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  
+  factory :user do
+    username { "test" }
+    email { "test@gmail.com" }
+    password { "123456" }
+  end
+
+  factory :post do
+    description { "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus atque quia nulla a odit hic? Cumque voluptatum sint voluptate. Minima natus quam nobis pariatur ipsa magnam deleniti voluptates, sed maiores?" }
+    association :user
+  end
+end
