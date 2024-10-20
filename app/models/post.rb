40c2b0c4b6422d7297d4784367a31189a09694e2
Carla Valdivia <valdivia.carla@gmail.com>
2024-10-20 20:37:23 -0300

    Fix post delete - comments dependent destroy

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   
   belongs_to :user
   has_many :likes
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_one_attached :photo
   validates :description, presence: true
   validates :description,  length: { in: 1..280 }
