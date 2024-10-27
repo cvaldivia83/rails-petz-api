1ce0f0481438709211fadd0b24aed2c65c8f1879
Carla Valdivia <valdivia.carla@gmail.com>
2024-10-26 23:00:32 -0300

    Add new params rule to post index action

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::PostsController < Api::V1::BaseController
   before_action :set_post, only: [ :show, :update, :destroy ]
-
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @posts = policy_scope(Post)
+
+    if params[:posts_number].present? 
+      @posts = @posts.order(created_at: :desc).limit(params[:posts_number].to_i)
+    end
 
     if params[:user_id].present? && params[:posts_number].present?
       @posts = @posts.where(user: params[:user_id].to_i)
