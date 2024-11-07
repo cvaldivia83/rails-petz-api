b39ae659978a8cbcbeeea3b2798822c09c0a8f6a
Carla Valdivia <valdivia.carla@gmail.com>
2024-11-07 17:58:07 -0300

    post seeds with photos

--- Gemfile
+++ Gemfile
@@ -12,6 +12,9 @@ gem 'pg', '~> 1.2'
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
 
+# Generator for seeds
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'main'
+
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 # gem "jbuilder"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,72 @@
 require 'open-uri'
+require "json"
+require 'faker'
+
+
+
+puts "Started seeds"
+
+
+User.destroy_all
+
+puts "Destroyed all records"
+
+puts "Creating users..."
+
+
 unsplash_url = 'https://api.unsplash.com/'
-client_id = "?client_id=#{ENV['UNSPLASH_ACCESS_KEY']}"
+client_id = "client_id=#{ENV['UNSPLASH_ACCESS_KEY']}"
 
 users = %w[ Inti Tula Juniper Petunia Mimosa Bolt Bolinha Athos Rex Farofa ]
-https://api.unsplash.com/photos/KZv7w34tluA?client_id=MZBmhlg2xVdmfdNYoaHHp7V7vrMA2abVmcb0V4ldB7I
 
-users.each do |user|
-  user = User.new(
+photo_ids = %w[ zv9k2ecE6lE KZv7w34tluA tIfrzHxhPYQ wUluw5KzT-s RxHhxWnXmNs tnGEuzqaDDg K4mSJ7kc0As m3PD1h17M yfLG3qsjwEo 75715CVEJhI ]
+
+users.each_with_index do |user, index|
+  
+  new_user = User.new(
     email: "#{user.downcase}@petz.com",
     password: 123456,
     username: user
   )
+
+  url = "#{unsplash_url}photos/#{photo_ids[index]}?#{client_id}"
+  photo_serialized = URI.parse(url).read
+  photo_hash = JSON.parse(photo_serialized)
+  photo_url = photo_hash['urls']['regular']
+
+  file = URI.parse(photo_url).open
+
+  new_user.avatar.attach(io: file, filename: "pet_#{index + 1}.jpg", content_type: 'image/jpg')
+
+  new_user.save!
+
+  puts "Created #{user} - user n.#{index + 1}"
+
+  sleep(2)
 end
 
-users_photos = %w[ zv9k2ecE6lE KZv7w34tluA tIfrzHxhPYQ wUluw5KzT-s RxHhxWnXmNs tnGEuzqaDDg K4mSJ7kc0As m3PD1h17M yfLG3qsjwEo 75715CVEJhI ]
+puts "Creating 3 posts for each user..."
 
+all_users = User.all
 
+all_users.each_with_index do |user, index|
 
-# user 
-user = User.new(
-  email: 'carla@lewagon.com',
-  password: '123456',
-  username: 'cvaldivia'
-)
+  
 
-file = URI.parse("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg").open
+  post = Post.new(
+    description: Faker::Lorem.sentences.join(" "),
+    user: user
+  )
 
-user.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+  url = "#{unsplash_url}/photos/random?orientation=portrait&query=pet&client_id=#{client_id}"
+  photo_serialized = URI.parse(url).read
+  photo_hash = JSON.parse(photo_serialized)
+  photo_url = photo_hash['urls']['regular']
 
-user.save!
+  post.avatar.attach(io: file, filename: "post_#{index + 1}.jpg", content_type: 'image/jpg')
+
+  post.save!
+
+end
+
+puts 'Seeds are over!'
