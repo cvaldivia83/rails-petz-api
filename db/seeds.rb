e5f3d53e9d9d69cddb07989ec21974f5fb430808
Carla Valdivia <valdivia.carla@gmail.com>
2024-11-07 14:54:09 -0300

    Started users seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,21 @@
 require 'open-uri'
+unsplash_url = 'https://api.unsplash.com/'
+client_id = "?client_id=#{ENV['UNSPLASH_ACCESS_KEY']}"
+
+users = %w[ Inti Tula Juniper Petunia Mimosa Bolt Bolinha Athos Rex Farofa ]
+https://api.unsplash.com/photos/KZv7w34tluA?client_id=MZBmhlg2xVdmfdNYoaHHp7V7vrMA2abVmcb0V4ldB7I
+
+users.each do |user|
+  user = User.new(
+    email: "#{user.downcase}@petz.com",
+    password: 123456,
+    username: user
+  )
+end
+
+users_photos = %w[ zv9k2ecE6lE KZv7w34tluA tIfrzHxhPYQ wUluw5KzT-s RxHhxWnXmNs tnGEuzqaDDg K4mSJ7kc0As m3PD1h17M yfLG3qsjwEo 75715CVEJhI ]
+
+
 
 # user 
 user = User.new(
