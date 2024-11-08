424bdd6eb22f2bd2639e3b09b3840809e92ba2ac
Carla Valdivia <valdivia.carla@gmail.com>
2024-11-07 23:42:35 -0300

    Seeds for users, posts and comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,9 @@ require 'faker'
 
 puts "Started seeds"
 
+Comment.destroy_all
 Post.destroy_all
-User.destroy_all
+# User.destroy_all
 
 puts "Destroyed all records"
 
@@ -19,31 +20,32 @@ client_id = "client_id=#{ENV['UNSPLASH_ACCESS_KEY']}"
 
 users = %w[ Inti Tula Juniper Petunia Mimosa Bolt Bolinha Athos Rex Farofa ]
 
-photo_ids = %w[ zv9k2ecE6lE KZv7w34tluA tIfrzHxhPYQ wUluw5KzT-s RxHhxWnXmNs tnGEuzqaDDg K4mSJ7kc0As m3PD1h17M yfLG3qsjwEo 75715CVEJhI ]
+photo_ids = %w[ zv9k2ecE6lE KZv7w34tluA tIfrzHxhPYQ wUluw5KzT-s RxHhxWnXmNs tnGEuzqaDDg K4mSJ7kc0As fglUYfIgmBA yfLG3qsjwEo 75715CVEJhI ]
 
-users.each_with_index do |user, index|
+# users.each_with_index do |user, index|
   
-  new_user = User.new(
-    email: "#{user.downcase}@petz.com",
-    password: 123456,
-    username: user
-  )
+#   new_user = User.new(
+#     email: "#{user.downcase}@petz.com",
+#     password: 123456,
+#     username: user
+#   )
 
-  url = "#{unsplash_url}photos/#{photo_ids[index]}?#{client_id}"
-  photo_serialized = URI.parse(url).read
-  photo_hash = JSON.parse(photo_serialized)
-  photo_url = photo_hash['urls']['regular']
+#   url = "#{unsplash_url}photos/#{photo_ids[index]}?#{client_id}"
+#   photo_serialized = URI.parse(url).read
+#   photo_hash = JSON.parse(photo_serialized)
+#   photo_url = photo_hash['urls']['regular']
 
-  file = URI.parse(photo_url).open
+#   file = URI.parse(photo_url).open
 
-  new_user.avatar.attach(io: file, filename: "pet_#{index + 1}.jpg", content_type: 'image/jpg')
+#   new_user.avatar.attach(io: file, filename: "pet_#{index + 1}.jpg", content_type: 'image/jpg')
 
-  new_user.save!
+#   new_user.save!
 
-  puts "Created #{user} - user n.#{index + 1}"
+#   puts "Created #{user} - user n.#{index + 1}"
 
-  sleep(2)
-end
+# end
+
+
 
 puts "Creating 3 posts for each user..."
 
@@ -52,25 +54,27 @@ all_users = User.all
 all_users.each_with_index do |user, index|
 
   3.times do |i|
+
     post = Post.new(
       description: Faker::Lorem.sentences.join(" "),
       user: user
     )
-  
-    url = "#{unsplash_url}/photos/random?orientation=portrait&query=pet&client_id=#{client_id}"
+
+    url = "#{unsplash_url}photos/random?orientation=portrait&query=pet&#{client_id}"
     photo_serialized = URI.parse(url).read
     photo_hash = JSON.parse(photo_serialized)
     photo_url = photo_hash['urls']['regular']
+
+    file = URI.parse(photo_url).open
   
     post.photo.attach(io: file, filename: "post_#{user.username}_#{i + 1}.jpg", content_type: 'image/jpg')
   
     post.save!
-
+    
     if i == 2
-      puts "Created 3 posts from #{user.username}"
+      puts "Created 3 posts for user #{user.username}"
     end
   end
-
 end
 
 puts "Posts created"
